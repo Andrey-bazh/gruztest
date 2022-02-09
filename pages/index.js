ad1023e44a2bbe2daabd5f7bd54f91fb57ce1869
andrey <bazh.andrey8@gmail.com>
2022-02-09 17:16:49 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import PropTypes from 'prop-types'
+
+const useStyles = makeStyles({
+	title: {
+		color: 'red',
+	},
+})
+
+export default function Page({ id, options, count, color, data }) {
+	return (
+		<MyWonderfulComponent
+			id={id}
+			options={options}
+			count={count}
+			color={color}
+			data={data}
+		>
+			Im text from a component
+		</MyWonderfulComponent>
+	)
+}
+
+function MyWonderfulComponent({ id, options, children, ...other }) {
+	const { count } = other
+	const [sum, setSum] = useState(count)
+	const classes = useStyles()
+
+	useEffect(() => {
+		if (id && options?.params?.fields?.isDynamic) {
+			setSum(sum + 1)
+		}
+	}, [])
+
+	return (
+		<>
+			<h1 className={classes.title}>Hello World!</h1>
+			<Grid>
+				<Grid item xs={12}>
+					{children}
+				</Grid>
+				<Grid item xs={12}>
+					{sum}
+				</Grid>
+			</Grid>
+		</>
+	)
+}
+
+export async function getStaticProps() {
+	const text = 'Hello from SSR'
+	console.log(text)
+	return {
+		props: {
+			id: 1,
+			options: {
+				params: {
+					fields: {
+						isDynamic: true,
+					},
+				},
+			},
+			count: 1,
+			color: 'red',
+			data: {},
+			text,
+		},
+	}
+}
